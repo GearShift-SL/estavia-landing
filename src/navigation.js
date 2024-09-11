030e0670b1efeb95a82b3895ecb02f29175f52fd
Daniel Garcia Sanchez <dontic@zoho.com>
2024-09-11 11:30:30 +0000

    Update index and navigation

--- src/navigation.js
+++ src/navigation.js
@@ -3,12 +3,12 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'Services',
-      href: '/#services',
+      text: 'Características',
+      href: '/#caracteristicas',
     },
     {
-      text: 'Projects',
-      href: getPermalink('/category/projects'),
+      text: 'FAQ',
+      href: '/#caracteristicas',
     },
     {
       text: 'Blog',
@@ -16,7 +16,7 @@ export const headerData = {
     },
   ],
   actions: [{
-    text: 'Get In Touch', variant: "primary",
+    text: 'Pruébalo gratis', variant: "primary",
     icon: 'tabler:rocket',
     href: getPermalink("/inquiry"),
     // target: '_blank'
@@ -26,36 +26,34 @@ export const headerData = {
 export const footerData = {
   links: [
     {
-      title: 'Support',
+      title: 'Soporte',
       links: [
-        { text: 'Contact', href: getPermalink('/contact') },
+        { text: 'Contacto', href: getPermalink('/contact') },
         { text: 'Blog', href: getBlogPermalink() },
-        { text: 'Guides', href: getPermalink('/category/guides') },
+        { text: 'Guías', href: getPermalink('/category/guides') },
       ],
     },
     {
-      title: 'Company',
+      title: 'Compañía',
       links: [
-        { text: 'Services', href: '/#services' },
-        { text: 'About', href: '/#about' },
-        { text: 'Past Projects', href: getPermalink('/category/projects') },
+        { text: 'Servicios', href: '/#services' },
+        { text: 'Sobre nosotros', href: '/#about' },
 
       ],
     },
   ],
   secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+    { text: 'Términos y condiciones', href: getPermalink('/terms') },
+    { text: 'Política de privacidad', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/gearshift' },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/GearShift' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/autovisita' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
   ],
   footNote: `
   <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
-  >© 2024 <a href="https://gearshift.es/" class="hover:underline"
-    >GearShift</a
+  >© 2024 <a href="https://autovisita.es/" class="hover:underline"
+    >AutoVisita</a
   >
 </span>
   `,
