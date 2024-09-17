0572d6a5025a12f2ceb20019ff0d741158587ca9
Daniel Garcia Sanchez <dontic@zoho.com>
2024-09-17 18:55:54 +0000

    Refactor header link text in navigation.js

--- src/navigation.js
+++ src/navigation.js
@@ -3,7 +3,7 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: '¿Cómo funciona?',
+      text: 'Descúbrelo',
       href: '/#transformation',
     },
     {
