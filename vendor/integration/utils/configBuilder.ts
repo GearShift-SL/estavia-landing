0e02adae85425de67151d3d410934bbb356a5f85
Daniel Garcia Sanchez <dontic@zoho.com>
2024-09-17 17:40:36 +0000

    Update non-src files

--- vendor/integration/utils/configBuilder.ts
+++ vendor/integration/utils/configBuilder.ts
@@ -1,85 +1,88 @@
-import merge from 'lodash.merge'
+import merge from 'lodash.merge';
 
-import type { MetaData } from '~/types'
+import type { MetaData } from '~/types';
 
-type Config = {
-  site?: SiteConfig
-  metadata?: MetaDataConfig
-  i18n?: I18NConfig
+export type Config = {
+  site?: SiteConfig;
+  metadata?: MetaDataConfig;
+  i18n?: I18NConfig;
   apps?: {
-    blog?: AppBlogConfig
-  }
-  ui?: unknown
-  analytics?: unknown
-}
+    blog?: AppBlogConfig;
+  };
+  ui?: unknown;
+  analytics?: unknown;
+};
 
 export interface SiteConfig {
-  name: string
-  site?: string
-  base?: string
-  trailingSlash?: boolean
-  googleSiteVerificationId?: string
+  name: string;
+  site?: string;
+  base?: string;
+  trailingSlash?: boolean;
+  googleSiteVerificationId?: string;
 }
 export interface MetaDataConfig extends Omit<MetaData, 'title'> {
   title?: {
-    default: string
-    template: string
-  }
+    default: string;
+    template: string;
+  };
 }
 export interface I18NConfig {
-  language: string
-  textDirection: string
-  dateFormatter?: Intl.DateTimeFormat
+  language: string;
+  textDirection: string;
+  dateFormatter?: Intl.DateTimeFormat;
 }
 export interface AppBlogConfig {
-  isEnabled: boolean
-  postsPerPage: number
-  isRelatedPostsEnabled: boolean
-  relatedPostsCount: number
+  isEnabled: boolean;
+  postsPerPage: number;
+  isRelatedPostsEnabled: boolean;
+  relatedPostsCount: number;
   post: {
-    isEnabled: boolean
-    permalink: string
+    isEnabled: boolean;
+    permalink: string;
     robots: {
-      index: boolean
-      follow: boolean
-    }
-  }
+      index: boolean;
+      follow: boolean;
+    };
+  };
   list: {
-    isEnabled: boolean
-    pathname: string
+    isEnabled: boolean;
+    pathname: string;
     robots: {
-      index: boolean
-      follow: boolean
-    }
-  }
+      index: boolean;
+      follow: boolean;
+    };
+  };
   category: {
-    isEnabled: boolean
-    pathname: string
+    isEnabled: boolean;
+    pathname: string;
     robots: {
-      index: boolean
-      follow: boolean
-    }
-  }
+      index: boolean;
+      follow: boolean;
+    };
+  };
   tag: {
-    isEnabled: boolean
-    pathname: string
+    isEnabled: boolean;
+    pathname: string;
     robots: {
-      index: boolean
-      follow: boolean
-    }
-  }
+      index: boolean;
+      follow: boolean;
+    };
+  };
 }
 export interface AnalyticsConfig {
   vendors: {
     googleAnalytics: {
-      id?: string
-    }
-  }
+      id?: string;
+      partytown?: boolean;
+    };
+  };
 }
 
-export interface UIConfig {}
+export interface UIConfig {
+  theme: string;
+}
 
-const DEFAULT_SITE_NAME = 'Website'
+const DEFAULT_SITE_NAME = 'Website';
 
 const getSite = (config: Config) => {
   const _default = {
@@ -88,43 +91,43 @@ const getSite = (config: Config) => {
     base: '/',
     trailingSlash: false,
 
-    googleSiteVerificationId: ''
-  }
+    googleSiteVerificationId: '',
+  };
 
-  return merge({}, _default, config?.site ?? {}) as SiteConfig
-}
+  return merge({}, _default, config?.site ?? {}) as SiteConfig;
+};
 
 const getMetadata = (config: Config) => {
-  const siteConfig = getSite(config)
+  const siteConfig = getSite(config);
 
   const _default = {
     title: {
       default: siteConfig?.name || DEFAULT_SITE_NAME,
-      template: '%s'
+      template: '%s',
     },
     description: '',
     robots: {
       index: false,
-      follow: false
+      follow: false,
     },
     openGraph: {
-      type: 'website'
-    }
-  }
+      type: 'website',
+    },
+  };
 
-  return merge({}, _default, config?.metadata ?? {}) as MetaDataConfig
-}
+  return merge({}, _default, config?.metadata ?? {}) as MetaDataConfig;
+};
 
 const getI18N = (config: Config) => {
   const _default = {
     language: 'en',
-    textDirection: 'ltr'
-  }
+    textDirection: 'ltr',
+  };
 
-  const value = merge({}, _default, config?.i18n ?? {})
+  const value = merge({}, _default, config?.i18n ?? {});
 
-  return value as I18NConfig
-}
+  return value as I18NConfig;
+};
 
 const getAppBlog = (config: Config) => {
   const _default = {
@@ -137,57 +140,58 @@ const getAppBlog = (config: Config) => {
       permalink: '/blog/%slug%',
       robots: {
         index: true,
-        follow: true
-      }
+        follow: true,
+      },
     },
     list: {
       isEnabled: true,
       pathname: 'blog',
       robots: {
         index: true,
-        follow: true
-      }
+        follow: true,
+      },
     },
     category: {
       isEnabled: true,
       pathname: 'category',
       robots: {
         index: true,
-        follow: true
-      }
+        follow: true,
+      },
     },
     tag: {
       isEnabled: true,
       pathname: 'tag',
       robots: {
         index: false,
-        follow: true
-      }
-    }
-  }
+        follow: true,
+      },
+    },
+  };
 
-  return merge({}, _default, config?.apps?.blog ?? {}) as AppBlogConfig
-}
+  return merge({}, _default, config?.apps?.blog ?? {}) as AppBlogConfig;
+};
 
 const getUI = (config: Config) => {
   const _default = {
-    theme: 'system'
-  }
+    theme: 'system',
+  };
 
-  return merge({}, _default, config?.ui ?? {})
-}
+  return merge({}, _default, config?.ui ?? {});
+};
 
 const getAnalytics = (config: Config) => {
   const _default = {
     vendors: {
       googleAnalytics: {
-        id: undefined
-      }
-    }
-  }
+        id: undefined,
+        partytown: true,
+      },
+    },
+  };
 
-  return merge({}, _default, config?.analytics ?? {}) as AnalyticsConfig
-}
+  return merge({}, _default, config?.analytics ?? {}) as AnalyticsConfig;
+};
 
 export default (config: Config) => ({
   SITE: getSite(config),
@@ -195,5 +199,5 @@ export default (config: Config) => ({
   METADATA: getMetadata(config),
   APP_BLOG: getAppBlog(config),
   UI: getUI(config),
-  ANALYTICS: getAnalytics(config)
-})
+  ANALYTICS: getAnalytics(config),
+});
